3153b7e2b71c37a3839f4b9c6c4d989e811b2de1
DiniSilva <101589841+DiniSilva@users.noreply.github.com>
2023-05-23 11:15:15 +0100

    Criação das Páginas
    Protótipo

--- dev_web_react_trabfinal/src/App.js
+++ dev_web_react_trabfinal/src/App.js
@@ -5,7 +5,22 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <Loja/>
+        <div className='sec'>
+          <nav className='navbar fixed-top navbar-dark'>
+            <a class="navbar-brand" href=''><img src='https://cdn.icon-icons.com/icons2/2620/PNG/512/among_us_player_red_icon_156942.png' class="d-inline-block"></img>Amogus</a>
+              <div className='nav-links'>
+                  <ul>
+                    <li class="nav-item"><a class="nav-link" href=''>HOME</a></li>
+                    <li class="nav-item"><a class="nav-link" href=''>About</a></li>
+                    <li class="nav-item"><a class="nav-link" href=''>Course</a></li>
+                    <li class="nav-item"><a class="nav-link" href=''>IDK</a></li>
+                    <li class="nav-item"><a class="nav-link" href=''>HOE</a></li>
+                  </ul>
+              </div>
+              <div className='blur'></div> 
+          </nav>
+          <Loja/>
+        </div>
       </header>
     </div>
   );
--- dev_web_react_trabfinal/src/app/Loja.js
+++ dev_web_react_trabfinal/src/app/Loja.js
@@ -1,6 +1,7 @@
 import logo from '../logo.svg';
 import React, { Component } from 'react';
 import Cliente from './Cliente';
+import Reparacao from './Reparacao';
 
 class Loja extends Component {
     constructor(props) {
@@ -8,40 +9,36 @@ class Loja extends Component {
         this.state = { troca: 0 };
     }
 
-    trocaFunc(){
-        this.setState({troca: 0});
-    }
-
-    trocaCliente(){
-        this.setState({troca: 1});
-    }
+    trocaLoja(){ this.setState({troca: 0}); }
+    trocaCliente(){ this.setState({troca: 1}); }
+    trocaReparacao(){ this.setState({troca: 2}); }
 
     render() {
         switch (this.state.troca) {
             case 0:
                 return (
                     <>
-        
-                        <img src={logo} className="App-logo" alt="logo" />
-                            <p>
-                                Edit <code>src/App.js</code> and save to reload.
-                            </p>
-                        <a
-                            className="App-link"
-                            href="https://reactjs.org"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            Learn React
-                        </a>
-                        <button onClick={() => this.trocaCliente()}>LMAO</button>
-                    </>      
+                        <section className='loja'>
+                            <h1>Ullamco incididunt dolore pariatur adipisicing.</h1>
+                            <p>Velit sint pariatur tempor non et enim. Sint minim sunt sit ea qui duis deserunt. Eiusmod veniam aute labore eu incididunt dolor commodo elit incididunt reprehenderit cillum adipisicing incididunt. Eu labore eu consectetur duis irure elit quis pariatur ad elit. <br/>Deserunt voluptate officia qui excepteur qui Lorem occaecat exercitation sint eu id. Culpa aliqua eu deserunt cillum et anim occaecat officia cupidatat.</p>
+                            <a href='#' className='hero-btn' onClick={() => this.trocaCliente()}>CARALHO1!!!</a>
+                        </section>
+                    </>
                 );
             case 1:
                 return (
                     <Cliente
                         troca={this.state.troca}
-                        trocaFunc={() => (this.trocaFunc())}
+                        trocaLoja={() => (this.trocaLoja())}
+                        trocaReparacao={() => (this.trocaReparacao())}
+                    />
+                );
+            case 2:
+                return (
+                    <Reparacao
+                        troca={this.state.troca}
+                        trocaLoja={() => (this.trocaLoja())}
+                        trocaCliente={() => (this.trocaCliente())}
                     />
                 );
             default: console.log("FÓNIX!!!!!")
