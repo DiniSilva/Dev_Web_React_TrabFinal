fbf976c8b5ebef726851cc72ac6f448c9c73c11b
DiniSilva <101589841+DiniSilva@users.noreply.github.com>
2023-07-03 16:20:35 +0100

    Adição de Login e Logout
    Co-Authored-By: FranciscoCarvalho2001 <101589943+FranciscoCarvalho2001@users.noreply.github.com>

--- dev_web_react_trabfinal/src/app/Loja.js
+++ dev_web_react_trabfinal/src/app/Loja.js
@@ -20,8 +20,11 @@ class Loja extends Component {
             "CodPostal": "",
             "Email": "",
             "Telemovel": "",
-        }, password: ""
-            
+        }, password: "",
+        UserLogin: {
+            Email: "", 
+            Password: ""
+        }, isLogged: false  
     }
 
     async componentDidMount(){
@@ -31,18 +34,86 @@ class Loja extends Component {
         this.buscarDadosReparacao();
     }
 
+    /*----------------------------------------------------------------------------*/
+
+    handleInputChangeLogin = (event) => {
+        const { name, value } = event.target;
+        this.setState(prevState => ({
+            UserLogin: {
+                ...prevState.UserLogin,
+                [name]: value
+            }
+        }));
+    };
+
+    handleLogin = (event) => {
+        event.preventDefault();
+        const { UserLogin } = this.state;
+        console.log( UserLogin );
+        this.login(UserLogin);
+        this.setState({
+            UserLogin: {
+                Email: "",
+                Password: "",
+            }
+        });
+    };
+
+    login (obj){
+        var requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(obj)
+        };
+        
+        fetch("https://localhost:7294/api/ClientesAPI/login", requestOptions)
+        .then((response) => {
+            if (response.ok) {
+                this.state.isLogged = true;
+                console.log("LOGIN FEITO!", this.state.isLogged );
+            } else {
+                throw new Error("Login failed");
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    };
+
+    logout (){
+        var requestOptions = {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+        };
+
+        fetch("https://localhost:7294/api/ClientesAPI/logout", requestOptions)
+        .then((response) => {
+            if (response.ok) {
+                this.state.isLogged = false;
+                console.log("LOGOUT FEITO!", this.state.isLogged );
+            } else {
+                throw new Error("Logout failed");
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    };
+
+    /*----------------------------------------------------------------------------*/
+
     handleInputChange = (event) => {
         const { name, value } = event.target;
         if (name === "Password"){
             this.setState({ password: value});
         }
         this.setState(prevState => ({
-          novoCliente: {
+            novoCliente: {
             ...prevState.novoCliente,
             [name]: value
-          }
+            }
         }));
-    }
+    };
 
     handleSubmit = (event) => {
         event.preventDefault();
@@ -60,7 +131,7 @@ class Loja extends Component {
             "Telemovel": "",
           }, password: ""
         });
-    }
+    };
     
     /*----------------------------------------------------------------------------*/
 
@@ -205,6 +276,7 @@ class Loja extends Component {
     render() {
         //const {novoCliente}= this.state;
         let novoCliente = this.state.novoCliente;
+        let UserLogin = this.state.UserLogin;
         return (
             <>
                 <div className='sec'>
@@ -225,44 +297,64 @@ class Loja extends Component {
 
                 <div id="mySidebar" class="sidebar ">
                     <a href="javascript:void(0)" class="closebtn" onClick={() => this.closeNav()}>&times;</a>
-                    <h2>Criar Cliente</h2>
-                    <form onSubmit={this.handleSubmit}>
-                        <label>
-                        Nome:
-                        <input type="text" name="Nome" value={novoCliente.Nome} onChange={this.handleInputChange} />
-                        </label>
-                        <label>
-                        Morada:
-                        <input type="text" name="Morada" value={novoCliente.Morada} onChange={this.handleInputChange} />
-                        </label>
-                        <label>
-                        CodPostal:
-                        <input type="text" name="CodPostal" value={novoCliente.CodPostal} onChange={this.handleInputChange} />
-                        </label>
-                        <label>
-                        Email:
-                        <input type="email" name="Email" value={novoCliente.Email} onChange={this.handleInputChange} />
-                        </label>
-                        <label>
-                        Password:
-                        <input type="password" name="Password" value={this.state.password} onChange={this.handleInputChange} />
-                        </label>
-                        <label>
-                        Telemovel:
-                        <input type="text" name="Telemovel" value={novoCliente.Telemovel} onChange={this.handleInputChange} />
-                        </label>
-                        <label>
-                        NIF:
-                        <input type="text" name="Nif" value={novoCliente.Nif} onChange={this.handleInputChange} />
-                        </label>
-                        <button type="submit">Create</button>
-                    </form>
+                    {this.state.isLogged ? (
+                        <>
+                        <h2>Criar Cliente</h2>
+                        <form onSubmit={this.handleSubmit}>
+                            <label>
+                            Nome:
+                            <input type="text" name="Nome" value={novoCliente.Nome} onChange={this.handleInputChange} />
+                            </label>
+                            <label>
+                            Morada:
+                            <input type="text" name="Morada" value={novoCliente.Morada} onChange={this.handleInputChange} />
+                            </label>
+                            <label>
+                            CodPostal:
+                            <input type="text" name="CodPostal" value={novoCliente.CodPostal} onChange={this.handleInputChange} />
+                            </label>
+                            <label>
+                            Email:
+                            <input type="email" name="Email" value={novoCliente.Email} onChange={this.handleInputChange} />
+                            </label>
+                            <label>
+                            Password:
+                            <input type="password" name="Password" value={this.state.password} onChange={this.handleInputChange} />
+                            </label>
+                            <label>
+                            Telemovel:
+                            <input type="text" name="Telemovel" value={novoCliente.Telemovel} onChange={this.handleInputChange} />
+                            </label>
+                            <label>
+                            NIF:
+                            <input type="text" name="Nif" value={novoCliente.Nif} onChange={this.handleInputChange} />
+                            </label>
+                            <button type="submit">Create</button>
+                        </form>
+                        </>
+                    ) : (
+                        <>
+                        <h2>Login</h2>
+                        <form onSubmit={this.handleLogin}>
+                            <label>
+                            Email:
+                            <input type="email" name="Email" placeholder="Email" value={UserLogin.Email} onChange={this.handleInputChangeLogin} />
+                            </label>
+                            <label>
+                            Login:
+                            <input type="password" name="Password" placeholder="Password" value={UserLogin.Password} onChange={this.handleInputChangeLogin}/>
+                            </label>
+                            <button type="submit">Login</button>
+                        </form>
+                    </>
+                    )}
                 </div>
 
                 <section className='loja'>
                     <h1>Ullamco incididunt dolore pariatur adipisicing.</h1>
                     <p>Velit sint pariatur tempor non et enim. Sint minim sunt sit ea qui duis deserunt. Eiusmod veniam aute labore eu incididunt dolor commodo elit incididunt reprehenderit cillum adipisicing incididunt. Eu labore eu consectetur duis irure elit quis pariatur ad elit. <br/>Deserunt voluptate officia qui excepteur qui Lorem occaecat exercitation sint eu id. Culpa aliqua eu deserunt cillum et anim occaecat officia cupidatat.</p>
                     <a href='#' className='hero-btn' onClick={() => this.openNav()}>&#9776; Open Sidebar</a>
+                    <button onClick={() => this.logout()}>LOGOUT</button>
                 </section>
                 <Cliente
                 />
